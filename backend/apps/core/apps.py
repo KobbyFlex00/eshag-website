37a93c97d56e56ba020f36565382e76a7cf66f96
KobbyFlex00 <kobbyflex1516@gmail.com>
2026-09-11 21:05:38 +0000

    feat: brand logo integration, dynamic team loading, official contact email update, and clean production build

--- backend/apps/core/apps.py
+++ backend/apps/core/apps.py
@@ -1,7 +1,36 @@
 from django.apps import AppConfig
 
 
+def patched_base_context_copy(self):
+    obj = object.__new__(self.__class__)
+    obj.__dict__.update(self.__dict__)
+    if hasattr(self, 'dicts'):
+        obj.dicts = self.dicts[:]
+    return obj
+
+
+def patched_context_copy(self):
+    obj = object.__new__(self.__class__)
+    obj.__dict__.update(self.__dict__)
+    if hasattr(self, 'dicts'):
+        obj.dicts = self.dicts[:]
+    return obj
+
+
+def patched_request_context_copy(self):
+    obj = object.__new__(self.__class__)
+    obj.__dict__.update(self.__dict__)
+    if hasattr(self, 'dicts'):
+        obj.dicts = self.dicts[:]
+    return obj
+
+
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.core'
-    label = 'core'
+
+    def ready(self):
+        from django.template import context
+        context.BaseContext.__copy__ = patched_base_context_copy
+        context.Context.__copy__ = patched_context_copy
+        context.RequestContext.__copy__ = patched_request_context_copy
